9326ce221658906be902fce7f671fc87363bbb52
Martin Dub <oakk.martin@gmail.com>
2023-11-08 21:26:31 +0100

    draft

--- database/migrations/2023_06_06_000000_create_bottles_table.php
+++ database/migrations/2023_06_06_000000_create_bottles_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bottles', function (Blueprint $table) {
-            $table->string('name', 18)->unique();
+            $table->id();
+            $table->string('name', 255);
             $table->string('filter')->nullable();
             $table->string('since')->nullable();
             $table->string('distribution')->nullable();
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [VodkaController::class, 'index'])->name('homepage');
+Route::get('/bottles/{bottle}', [VodkaController::class, 'detail'])->name('bottle.detail');

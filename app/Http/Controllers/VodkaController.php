079c996757057b76d7bc5acabf78adfddde9a1f8
Martin Dub <martin.dub@dek-cz.com>
2024-01-08 12:48:21 +0100

    bug fix

--- app/Http/Controllers/VodkaController.php
+++ app/Http/Controllers/VodkaController.php
@@ -17,33 +17,17 @@ class VodkaController extends Controller
 
     public function index(Request $request): View
     {
-        $flavor = $request->input('flavors') ? 'Flavors & Premium' : null;
-        $cities = $request->input('cities') ? 'City series' : null;
-        $limited = $request->input('limited') ? 'Limited Edition' : null;
-        $sorted = $this->facade->getBottles();
-
-        $final = [];
-        if (request('flavors')) {
-            $final += array_filter($sorted, fn ($bottle) => $bottle->filter === $flavor);
-        }
-
-        if (request('cities')) {
-            $final += array_filter($sorted, fn ($bottle) => $bottle->filter === $cities);
-        }
-
-        if (request('limited')) {
-            $final += array_filter($sorted, fn ($bottle) => str_contains(strtolower($bottle->filter), strtolower($limited)));
-        }
-
         return view('home', [
-            'bottles' => count($final) > 0 ? $final : $sorted,
+            'bottles' => $this->facade->getBottles(),
         ]);
     }
 
     public function show(string $bottle): View
     {
         $data = $this->facade->getBottleByHandle($bottle);
-        return view('detail', compact('data'));
+        $bottles = $this->facade->getBottles();
+
+        return view('detail', compact(['data', 'bottles']));
     }
 
     public function create(): View

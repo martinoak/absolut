2f821474b66846e1e67242ebdaa91cc7d4072022
Martin Dub <martin.dub@dek-cz.com>
2024-01-09 18:41:29 +0100

    orderBy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,7 +19,7 @@ class AdminController extends Controller
 
     public function db(): View
     {
-        $bottles = DB::table('bottles')->get()->toArray();
+        $bottles = DB::table('bottles')->orderBy('name')->get()->toArray();
         return view('admin.db', compact('bottles'));
     }
 }

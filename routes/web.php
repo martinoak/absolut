c928f081752e5723b35a6ac3e7ade8b3122d0348
Martin Dub <49438284+martinoak@users.noreply.github.com>
2024-04-29 09:55:01 +0200

    Apple icon

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MixologyController;
-use App\Http\Controllers\SetsController;
 use App\Http\Controllers\VodkaController;
 use Illuminate\Support\Facades\Route;
 
